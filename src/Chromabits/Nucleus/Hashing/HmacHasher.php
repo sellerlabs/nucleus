68ff82e8417b7f5f56ef79d1ed3068c65b622c82
Eduardo Trujillo <ed@chromabits.com>
2015-07-30 14:45:00 -0400

    Add HmacHasher class

---
+++ src/Chromabits/Nucleus/Hashing/HmacHasher.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Chromabits\Nucleus\Hashing;
+
+use Chromabits\Nucleus\Meditation\Arguments;
+use Chromabits\Nucleus\Meditation\Constraints\PrimitiveTypeConstraint;
+use Chromabits\Nucleus\Meditation\Primitives\ScalarTypes;
+
+/**
+ * Class HmacHasher
+ *
+ * @author Eduardo Trujillo <ed@chromabits.com>
+ * @package Chromabits\Nucleus\Hashing
+ */
+class HmacHasher
+{
+    /**
+     * Algorithm to use for hashing.
+     *
+     * @var string
+     */
+    protected $algorithm;
+
+    /**
+     * Construct a new instance of a HmacHasher.
+     *
+     * @param string $algorithm
+     */
+    public function __construct($algorithm = 'sha512')
+    {
+        $this->algorithm = $algorithm;
+    }
+
+    /**
+     * Get a list of supported hashing algorithms.
+     *
+     * @return array
+     */
+    public static function getAlgorithms()
+    {
+        return hash_algos();
+    }
+
+    /**
+     * Generate a hash of the content using the provided private key.
+     *
+     * @param string $content
+     * @param string $privateKey
+     *
+     * @return string
+     */
+    public function hash($content, $privateKey)
+    {
+        Arguments::contain(
+            PrimitiveTypeConstraint::forType(ScalarTypes::SCALAR_STRING),
+            PrimitiveTypeConstraint::forType(ScalarTypes::SCALAR_STRING)
+        )->check($content, $privateKey);
+
+        return hash_hmac($this->algorithm, $content, $privateKey);
+    }
+
+    /**
+     * Verify that a hash is valid.
+     *
+     * @param string $hash
+     * @param string $content
+     * @param string $privateKey
+     *
+     * @return bool
+     */
+    public function verify($hash, $content, $privateKey)
+    {
+        return $this->hash($content, $privateKey) === $hash;
+    }
+}
