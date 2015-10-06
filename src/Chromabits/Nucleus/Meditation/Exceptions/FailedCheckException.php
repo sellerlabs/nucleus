785a52252dbc0069040013ea5cb94c197c96ae2f
Eduardo Trujillo <ed@chromabits.com>
2015-10-06 14:42:51 -0400

    Run `phpstd clean` (again)

--- src/Chromabits/Nucleus/Meditation/Exceptions/FailedCheckException.php
+++ src/Chromabits/Nucleus/Meditation/Exceptions/FailedCheckException.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Copyright 2015, Eduardo Trujillo
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This file is part of the Nucleus package
+ */
+
 namespace Chromabits\Nucleus\Meditation\Exceptions;
 
 use Chromabits\Nucleus\Exceptions\CoreException;
@@ -8,7 +17,7 @@ use Chromabits\Nucleus\Meditation\Interfaces\CheckResultInterface;
 use Exception;
 
 /**
- * Class FailedSpecException
+ * Class FailedSpecException.
  *
  * Thrown when a check fails (most commonly a Spec or Validator). This
  * exception is available for cases when it is useful to raise the failed
