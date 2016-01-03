27ecad4c05b7a650bd12d9911af3bdfdb5f746a8
Eduardo Trujillo <ed@chromabits.com>
2016-01-03 14:45:17 -0500

    Implement Option and Select node classes

---
+++ src/Chromabits/Nucleus/View/Common/Select.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Copyright 2015, Eduardo Trujillo
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This file is part of the Nucleus package
+ */
+
+namespace Chromabits\Nucleus\View\Common;
+
+use Chromabits\Nucleus\View\Interfaces\RenderableInterface;
+use Chromabits\Nucleus\View\Node;
+
+/**
+ * Class Select.
+ *
+ * @author Eduardo Trujillo <ed@chromabits.com>
+ * @package Chromabits\Nucleus\View\Common
+ */
+class Select extends Node
+{
+    /**
+     * Construct an instance of a Select.
+     *
+     * @param string[] $attributes
+     * @param string|RenderableInterface|string[]|RenderableInterface[] $content
+     */
+    public function __construct($attributes, $content = '')
+    {
+        parent::__construct('select', $attributes, $content);
+    }
+}
