7c75e5179162c6129a5d6342048ba247120d97de
Eduardo Trujillo <ed@chromabits.com>
2016-01-20 00:45:32 -0500

    Add old Enum class back to reduce breaking changes

---
+++ src/Chromabits/Nucleus/Support/Enum.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Chromabits\Nucleus\Support;
+
+use Chromabits\Nucleus\Foundation\Enum as BaseEnum;
+
+/**
+ * Class Enum
+ *
+ * @author Eduardo Trujillo <ed@chromabits.com>
+ * @deprecated Use Chromabits\Nucleus\Foundation\Enum instead
+ * @package Chromabits\Nucleus\Support
+ */
+class Enum extends BaseEnum
+{
+    //
+}
